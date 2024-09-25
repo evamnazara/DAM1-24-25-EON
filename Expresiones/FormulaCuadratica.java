bab6d75269abbed53e7227a72add9f7940fa9f52
Eva Otero <eoternaza@ieschandomonte.edu.es>
2024-09-25 14:20:47 +0200

    ahora si

---
+++ Expresiones/FormulaCuadratica.java
@@ -0,0 +1,14 @@
+package Expresiones;
+import java.util.*;
+
+public class FormulaCuadratica {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double discriminante = Math.pow(b, 2) -4 * a * c;
+
+    }
+}
