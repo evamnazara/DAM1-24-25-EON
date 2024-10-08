f5dfa5160fc9134f90e0f58ac92c20571588e541
Eva Otero <eoternaza@ieschandomonte.edu.es>
2024-10-08 14:19:39 +0200

    correccion 2

--- src/ud1/ejercicios/one20241004/EP0123.java
+++ src/ud1/ejercicios/one20241004/EP0123.java
@@ -11,17 +11,20 @@ public class EP0123 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
 
+        final double GRAMOS_LIBRA = 453.592;
+
         System.out.println("Este programa convierte Kilogramos a Libras.");
-        System.out.println("Primero introduce la cantidad de masa, sin unidades");
-        double cantidad = sc.nextDouble();
+        System.out.println("Primero introduce la cantidad de masa, sin unidades: ");
+        double masaOrigen = sc.nextDouble();
 
         System.out.println("Ahora, introduce la unidad de medida [kilogramos (1) o libras (2)]");
-        int unidad = sc.nextInt();
+        int unidadMedida = sc.nextInt();
 
-        double resultado = unidad == 1 ? cantidad * 2.20462 : cantidad / 2.20462;
-        int conversion = unidad == 1 ? 2 : 1;
+        double resultado = unidadMedida == 1 ? masaOrigen * 1000 / GRAMOS_LIBRA : masaOrigen * GRAMOS_LIBRA / 1000; //si unidad es 1 es kg y multiplica, si no el contrario
+        int conversion = unidadMedida == 1 ? 2 : 1;
 
-        System.out.println(cantidad + (unidad == 1 ? "kg" : "lb") + " equivalen a " + resultado + " " + (conversion == 1 ? "kg" : "lb"));
+       System.out.println(masaOrigen + (unidadMedida == 1 ? "kg" : "lb") + " equivalen a " + resultado + " " + (conversion == 1 ? "kg" : "lb"));
+       //System.out.println("El resultado es %.3f %s " + conversion);
 
         sc.close();
 
