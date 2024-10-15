eeea2d289768f1e457b6a018e642a9dea3adfda8
Eva Otero <eoternaza@ieschandomonte.edu.es>
2024-10-15 22:11:36 +0200

    otro

--- src/examenesanteriores/Hectareas.java
+++ src/examenesanteriores/Hectareas.java
@@ -1,5 +1,13 @@
 package examenesanteriores;
-
+import java.util.*;
 public class Hectareas {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int HECTAREA = 10000; // 100 x 100 m2 
+        System.out.println("introduce el número de hectáreas que quieres convertir:");
+
+        sc.close();
+
+    }
 }
