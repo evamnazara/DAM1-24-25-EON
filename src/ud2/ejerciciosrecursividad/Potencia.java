e7c081ed2466a9ae70fdce92863818edb0b97504
evamnazara <eoternaza@ieschandomonte.edu.es>
2024-11-13 14:28:28 +0100

    13 nov 2

---
+++ src/ud2/ejerciciosrecursividad/Potencia.java
@@ -0,0 +1,12 @@
+/* Potencia. Escribe un programa que calcule la potencia de un número elevado a otro utilizando recursividad.
+ */
+
+package ud2.ejerciciosrecursividad;
+
+public class Potencia {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
