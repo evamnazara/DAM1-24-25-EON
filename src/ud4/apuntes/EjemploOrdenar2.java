eaf27ba80d1628dfc1b9524c622806bb9d5e8ec1
evamnazara <eoternaza@ieschandomonte.edu.es>
2025-01-09 11:23:45 +0100

    ordenar Arrays

--- src/ud4/apuntes/EjemploOrdenar2.java
+++ src/ud4/apuntes/EjemploOrdenar2.java
@@ -2,6 +2,35 @@
 
 package ud4.apuntes;
 
+import java.util.Arrays;
+import ud4.ArraysUtil;
+
 public class EjemploOrdenar2 {
-    
+
+    public static void seleccion(int A[]) {
+        int i, j, menor, pos, tmp;
+        for (i = 0; i < A.length - 1; i++) {      // tomamos como menor el primero
+              menor = A[i];                       // de los elementos que quedan por ordenar                    
+              pos = i;                            // y guardamos su posición
+              for (j = i + 1; j < A.length; j++){ // buscamos en el resto
+                    if (A[j] < menor) {           // del array algún elemento
+                        menor = A[j];             // menor que el actual
+                        pos = j;
+                    }
+              }
+              if (pos != i){                      // si hay alguno menor se intercambia                         
+                  tmp = A[i];
+                  A[i] = A[pos];
+                  A[pos] = tmp;
+              }
+        }
+}
+
+    public static void main(String[] args) {
+        int [] t = ArraysUtil.arrayAleatorio(10, 1, 10);
+        System.out.println(Arrays.toString(t)); // impresion del string a pelo 
+
+        seleccion(t); // funcion con el array
+        System.out.println(Arrays.toString(t)); // impresion del string ordenado 
+    }
 }
---
+++ src/ud4/apuntes/EjemploOrdenar5.java
@@ -0,0 +1,42 @@
+package ud4.apuntes;
+
+import java.util.Arrays;
+
+import ud4.ArraysUtil;
+
+public class EjemploOrdenar5 {
+    public static void mergesort(int A[],int izq, int der){
+        if (izq < der){
+                int m=(izq+der)/2;
+                mergesort(A,izq, m);
+                mergesort(A,m+1, der);                                                                                
+                merge(A,izq, m, der);                                                                                 
+        }
+    }
+
+    public static void merge(int A[],int izq, int m, int der){
+        int i, j, k;
+        int [] B = new int[A.length]; //array auxiliar
+        for (i=izq; i<=der; i++)      //copia ambas mitades en el array auxiliar                                       
+             B[i]=A[i];
+     
+        i=izq; j=m+1; k=izq;
+          
+        while (i<=m && j<=der) //copia el siguiente elemento más grande                                      
+               if (B[i]<=B[j])
+                   A[k++]=B[i++];
+               else
+                   A[k++]=B[j++];
+             
+        while (i<=m)         //copia los elementos que quedan de la
+              A[k++]=B[i++]; //primera mitad (si los hay)
+     }
+
+     public static void main(String[] args) {
+         int [] t = ArraysUtil.arrayAleatorio(10000, 1, 10);
+        System.out.println(Arrays.toString(t)); // impresion del string a pelo 
+
+        mergesort(t,0,t.length -1); // funcion con el array
+        System.out.println(Arrays.toString(t)); // impresion del string ordenado 
+     }
+}
