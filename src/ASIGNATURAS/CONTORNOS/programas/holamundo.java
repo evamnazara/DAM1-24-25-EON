2345076d2152ff059753ed53fdbbe56f3ac760e4
evamnazara <eoternaza@ieschandomonte.edu.es>
2025-01-22 09:50:28 +0100

    E

--- src/ASIGNATURAS/CONTORNOS/programas/holamundo.java
+++
@@ -1,7 +0,0 @@
-package Contornos.programas;
-public class holamundo {
- 
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo!!!");
-    }  
-}
--- src/ASIGNATURAS/CONTORNOS/programas/nombrej.java
+++
@@ -1,6 +0,0 @@
-package Contornos.programas;
-public class nombrej {
-    public static void main(String[] args) {
-        System.out.println("EMON - 17, YOM - 15");
-    }  
-}
