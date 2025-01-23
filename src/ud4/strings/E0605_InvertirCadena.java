35bfe1e8314ec428e742681141e29ace0f004d03
evamnazara <eoternaza@ieschandomonte.edu.es>
2025-01-23 12:51:14 +0100

    programacion 23 de enero

---
+++ src/ud4/strings/E0605_InvertirCadena.java
@@ -0,0 +1,35 @@
+/*E0605. Diseña una función a la que se le pase una cadena de caracteres y la devuelva 
+invertida. Por ejemplo: la cadena “Hola mundo” se devolvería como “odnum aloH”. 
+ 
+Puedes implementar un método con el siguiente prototipo: 
+ 
+static String invertirCadena(String cad)  */
+
+package ud4.strings;
+import java.util.Scanner;
+
+public class E0605_InvertirCadena {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Frase:");
+        String frase = sc.nextLine();
+        
+       // String fraseInvertida = invertirCadena(frase);
+
+
+    }
+
+   /* static String invertirCadena(String cad) {
+        String cadenaInvertida;
+
+        //concatenar en una cadena vacía 
+        //palabra palindromo 
+
+        for (int i = 0; i < cad.length(); i++ ){
+        }
+
+        return cadenaInvertida;
+    }*/ 
+}
