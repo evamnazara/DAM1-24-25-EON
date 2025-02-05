bfcede17409813e29114654272c201ceb7073a55
evamnazara <eoternaza@ieschandomonte.edu.es>
2025-02-05 09:58:56 +0100

    bases 5 febrero

---
+++ src/ASIGNATURAS/BASES/SQL/UD4A3_Tarea1_evamariaoteronazara.sql
@@ -0,0 +1,63 @@
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'BD_TiposUsuario1')
+    BEGIN
+    DROP DATABASE BD_TiposUsuario1;
+    END 
+    
+CREATE DATABASE BD_TiposUsuario1
+
+GO 
+exec sp_helpdb BD_TiposUsuario1
+GO 
+
+USE BD_TiposUsuario1
+GO
+
+-- TipoCodigo Datos de tipo carácter de 10 caracteres
+
+BEGIN
+	IF EXISTS(Select * from sys.types where name='TipoCodigo') 
+	DROP TYPE TipoCodigo
+END
+GO 
+
+CREATE TYPE TipoCodigo from char (10) NULL 
+Select * from sys.types where name='TipoCodigo'
+
+GO
+
+--TipoNum: Un número completo que no excederá 30000
+
+BEGIN
+	IF EXISTS(Select * from sys.types where name='TipoNum') 
+	DROP TYPE TipoNum
+END
+GO 
+--
+CREATE TYPE TipoNum from char (3000) 
+Select * from sys.types where name='TipoNum'
+GO 
+
+--TipoTelefono: Datos de tipo carácter de 9 caracteres que permita NULL
+BEGIN
+	IF EXISTS(Select * from sys.types where name='TipoTelefono') 
+	DROP TYPE TipoTelefono
+END
+GO 
+
+
+
+CREATE TYPE TipoTelefono from char (9) NULL 
+Select * from sys.types where name='TipoTelefono'
+GO 
+
+--TipoNombreCorto: Datos de tipo caracteres variables de hasta 15 caracteres
+
+BEGIN
+	IF EXISTS(Select * from sys.types where name='TipoNombreCorto') 
+	DROP TYPE TipoNombreCorto
+END
+GO 
+
+CREATE TYPE TipoNombreCorto from varchar (15)  
+Select * from sys.types where name='TipoNombreCorto'
+GO 
