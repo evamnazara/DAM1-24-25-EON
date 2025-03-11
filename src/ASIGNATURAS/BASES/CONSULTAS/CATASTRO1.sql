1fcc0430cb631dd9df81803a9c975279c3c52e76
Eva Otero <eoternaza@ieschandomonte.edu.es>
2025-03-11 21:53:39 +0100

    catastro terminado

--- src/ASIGNATURAS/BASES/CONSULTAS/CATASTRO1.sql
+++ src/ASIGNATURAS/BASES/CONSULTAS/CATASTRO1.sql
@@ -1,71 +1,74 @@
 
 
 -- 1. muestre el nombre, descripción y observaciones de las zonas urbanas.
-SELECT nombrezona, descripción, observaciones FROM ZONAURBANA;
-
+SELECT nombrezona, descripción, observaciones FROM ZONAURBANA
+go 
 
 -- 2. devuelva el nombre de la calle, número y número de plantas de las viviendas unifamiliares
-
-SELECT calle, numero, numplantas FROM CASAPARTICULAR;
-
+SELECT calle, numero, numplantas FROM CASAPARTICULAR
+go 
 -- 3. nombre de la calle, número y metros construidos de las viviendas unifamiliares que tienen piscina.
 
-SELECT calle, numero, metrosconstruidos FROM CASAPARTICULAR WHERE  PISCINA ='S';
+SELECT calle, numero, metrosconstruidos FROM CASAPARTICULAR WHERE  PISCINA ='S'
+go 
 
 -- 4. calle, número y metros del solar de las viviendas unifamiliares.  
-SELECT calle, numero, METROSSOLAR FROM VIVIENDA where TIPOVIVIENDA = 'Casa';
-
+SELECT calle, numero, METROSSOLAR FROM VIVIENDA where TIPOVIVIENDA = 'Casa'
+go 
 -- 5. información de los pisos que tienen 3 habitaciones.
 
-SELECT * FROM PISO WHERE NUMHABITACIONES = '3';
-
+SELECT * FROM PISO WHERE NUMHABITACIONES = '3'
+go 
 -- 6. obtener una relación de viviendas unifamiliares (calle, número y metros de solar) de aquellas viviendas en las que los metros de solar están entre 190 y 300 metros.
 
 SELECT calle, numero, METROSSOLAR FROM VIVIENDA 
 	WHERE METROSSOLAR BETWEEN 190 AND 300
-	AND TIPOVIVIENDA = 'Casa';
+	AND TIPOVIVIENDA = 'Casa'
+go 
 
 -- 7. relación de bloques de pisos que tienen más de 15 vecinos (pisos) ordenados por calle y número.
 
 	--SELECT * from BLOQUEPISOS where PROPIETARIO > 15 ;  
 
 -- 8. calle, número y metros del solar de las viviendas unifamiliares situadas en la zona centro.
-SELECT calle, numero, metrossolar FROM VIVIENDA WHERE NOMBREZONA = 'centro';
+SELECT calle, numero, metrossolar FROM VIVIENDA WHERE NOMBREZONA = 'centro'
+go 
 
 -- 9. devuelva DNI, nombre y apellidos de las personas que tienen López como primer apellido ordenadas por apellidos y nombre.
 
 SELECT DNI,NOMBRE,APELLIDO1,APELLIDO2 FROM PROPIETARIO 
-	WHERE APELLIDO1 = 'López' ORDER BY APELLIDO1;
-
+	WHERE APELLIDO1 = 'López' ORDER BY APELLIDO1
+go 
 
 -- 10. calle, número y metros del solar de los bloques de pisos situados en la zona Centro o Palomar que tienen mas de 450 metros de solar
 
 SELECT CALLE,NUMERO,METROSSOLAR FROM VIVIENDA 
 	WHERE (NOMBREZONA = 'CENTRO' OR NOMBREZONA = 'PALOMAR')
-		AND (METROSSOLAR > 450);
-
+		AND (METROSSOLAR > 450)
+go 
 
 -- 11. consulta que muestre los garajes que están sin vender ordenados por calle, número e id_hueco
 
 SELECT * FROM HUECO 
 	WHERE TIPO = 'GARAJE' AND DNIPROPIETARIO IS NULL
-	ORDER BY CALLE, NUMERO, ID_HUECO;
-
+	ORDER BY CALLE, NUMERO, ID_HUECO
+go 
 -- 12. nombre y descripción de las zonas urbanas que tienen más de 1 parque ordenadas por el número de parque descendente y nombre ascendente
 
 SELECT nombrezona, descripción FROM ZONAURBANA 
 	WHERE NUMPARQUES > 1 
-	ORDER BY NUMPARQUES desc;
-
+	ORDER BY NUMPARQUES desc
+go 
 
 -- 13. Mostrar toda la información de las zonas urbanas que tienen algo escrito en el campo de observaciones.
-select * from ZONAURBANA where OBSERVACIONES is not null;
+select * from ZONAURBANA where OBSERVACIONES is not null
+go 
 
 -- 14. consulta que devuelva DNI, nombre y apellidos de las personas que tienen un nombre de 3 letras, ordenado por nombre y apellidos 
 
 SELECT DNI, NOMBRE, APELLIDO1, APELLIDO2 FROM PROPIETARIO
- WHERE LEN(NOMBRE) = 3;
-
+ WHERE LEN(NOMBRE) = 3
+go 
  
 -- 15. que muestre calle, número, planta, puerta, metros (construidos y útiles) y la diferencia existente entre los metros construidos y los metros útiles ordenados por esta diferencia descendentemente
 
@@ -78,6 +81,7 @@ SELECT DNI, NOMBRE, APELLIDO1, APELLIDO2 FROM PROPIETARIO
 
 
 -- 18. nombres de zona y número de parques de las zonas que poseen menos de 3 parques y en los que el campo de observaciones está vacio.
+
 
 -- 19. muestre los pisos de dos habitaciones que hay en una calle que empiece por la letra “L” y que tienen menos de 100 metros útiles.
 
---
+++ src/ASIGNATURAS/BASES/CONSULTAS/CATASTRO2.sql
@@ -0,0 +1,60 @@
+
+-- 21. 25% de los pisos con más habitaciones. En el caso de haber más pisos con ese número de habitaciones deberían visualizarse también.
+SELECT TOP 25 PERCENT WITH TIES * FROM PISO 
+    ORDER BY NUMHABITACIONES
+go
+
+-- 22. toda la información de los garajes con al menos 14 metros. En caso de no tener propietario deberá mostrar desconocido.
+SELECT CODIGO, CALLE, NUMERO, PLANTA, PUERTA, ID_HUECO, TIPO, METROS, OBSERVACIONES, DNI_PROPIETARIO = ISNULL(DNIPROPIETARIO,'Desconocido') FROM HUECO 
+    WHERE METROS >= 14
+GO
+
+-- 23. nombre completo (p.e. Javier López Díaz) de los propietarios cuyo nombre no empiece por a, b, c, d o e cuyo apellido1 tenga más de 4 letras ordenados por sexo , nombre y apellidos.
+SELECT nombreCompleto = NOMBRE +' '+ APELLIDO1 +' '+ ISNULL(APELLIDO2,'') FROM PROPIETARIO 
+    WHERE NOMBRE LIKE'[^abcde]%' AND LEN(APELLIDO1)>4 
+    ORDER BY SEXO, NOMBRE, APELLIDO1, APELLIDO2
+GO
+
+-- 24. información de las viviendas de la calle Damasco o General Mola cuyos metros solar empiecen por 2.
+SELECT * FROM VIVIENDA 
+    WHERE CALLE IN('Damasco', 'General Mola') AND METROSSOLAR 
+    LIKE '[2]%'
+GO
+
+-- 25. para cada propietario el nombre, apellido1, sexo y un identificador que se creará concatenando el sexo con las 3 primeras letras del nombre y las dos últimas del apellido1.
+SELECT nombreCompleto = NOMBRE +' '+ APELLIDO1 +' '+ ISNULL(APELLIDO2,''), SEXO, identificador = SEXO+LEFT(NOMBRE,3)+RIGHT(APELLIDO1,2) FROM PROPIETARIO
+GO
+
+-- 26. muestre los distintos tipos de huecos que hay en la calle Sol o Luca de Tena.
+SELECT DISTINCT TIPO FROM HUECO 
+    WHERE CALLE IN('Sol', 'Luca de Tena')
+GO
+
+-- 27. información de los 5 huecos más pequeños. En el caso de que haya más cuyo tamaño sea igual deberán visualizarse todos.
+SELECT TOP 5 WITH TIES * FROM HUECO 
+    ORDER BY METROS
+GO
+
+-- 28. nombres de las mujeres con los caracteres invertidos.
+SELECT NOMBREINVERTIDO = REVERSE(NOMBRE) FROM PROPIETARIO 
+    WHERE SEXO = 'M'
+GO
+
+-- 29. los trasteros o garajes sin mostrar los decimales de los metros.
+SELECT metros = FLOOR(METROS) FROM HUECO 
+    WHERE TIPO IN('TRASTERO','GARAJE') 
+GO
+
+-- 30. distintos tipos de huecos de manera que se visualice la primera letra en mayúsculas y las siguientes en minúsculas.
+SELECT DISTINCT tipoHueco = LEFT(TIPO,1) + LOWER(SUBSTRING(TIPO,2,LEN(TIPO))) FROM HUECO 
+GO
+
+-- 31. nombre completo de los propietarios y sexo, indicando los valores Masculino o Femenino en función del valor del campo sexo
+SELECT nombreCompleto = NOMBRE +' '+ APELLIDO1 +' '+ ISNULL(APELLIDO2,''), 
+	sexo = CASE 
+		WHEN SEXO = 'M' THEN 'Femenino'
+		WHEN SEXO = 'H' THEN 'Masculino'
+		ELSE 'Desconocido'
+	FROM PROPIETARIO
+    END 
+GO
