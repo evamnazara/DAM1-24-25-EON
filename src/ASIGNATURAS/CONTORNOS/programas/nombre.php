8cd6e1ba5940873c5e6ce1c1ff309f3b64b3fdf1
evamnazara <eoternaza@ieschandomonte.edu.es>
2025-04-02 08:27:28 +0200

    e

--- src/ASIGNATURAS/CONTORNOS/programas/nombre.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Eva Maria Otero Nazara (EON - numero 20) - Yago Otero Martinez (YON - 19)";
-?>
---
+++ src/ud6/ejercicios/E1207.java
@@ -0,0 +1,7 @@
+package ud6.ejercicios;
+
+public class E1207 {
+    public static void main(String[] args) {
+        
+    }
+}
