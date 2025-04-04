cbb3d833ba322f48b19ed5bdeb27030ff0635e36
evamnazara <eoternaza@ieschandomonte.edu.es>
2025-04-04 14:27:37 +0200

    prog 04 abril y sistemas

--- src/ud6/ejercicios/E1208.java
+++ src/ud6/ejercicios/E1208.java
@@ -7,24 +7,15 @@ import java.util.Random;
 public class E1208 {
     public static void main(String[] args) {
         Collection <Integer> numeros = new ArrayList<>();
-<<<<<<< HEAD
     Random rnd = new Random();
 
-    for (int i = 0; i < 20; i++) {
-=======
         for (int i = 0; i < 20; i++) {
-            Random rnd = new Random();
->>>>>>> 24f1d8d321eea0220aea14adbecc71370f8907e8
             numeros.add(rnd.nextInt());
         }
         System.out.println(numeros);
-    }
-<<<<<<< HEAD
 
 }
 
        
 
-=======
 }
->>>>>>> 24f1d8d321eea0220aea14adbecc71370f8907e8
